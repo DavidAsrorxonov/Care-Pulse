d9dd73e402104a9fdc9df29a13e4ee8657f1be67
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-11 11:04:50 +0900

    get the userId as props from the params of the folder

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,9 +1,12 @@
 import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/cmd/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Register = () => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+
   return (
     <div className="flex h-screen max-h-screen">
       <section className="relative flex-1 overflow-y-auto px-[5%] my-auto">
