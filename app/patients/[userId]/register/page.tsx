3843a1829204f4b2be12926892416d23045094b1
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-12 11:14:45 +0900

    complete patient actions with all functions; fix bugs when registering

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Register = async ({ params: { userId } }: SearchParamProps) => {
+const Register = async (props: SearchParamProps) => {
+  const { userId } = await props.params;
   const user = await getUser(userId);
 
   return (
